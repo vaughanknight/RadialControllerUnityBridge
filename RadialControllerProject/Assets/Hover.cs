df6b79e033e719844f308d04ffde179afe129aaa
Vaughan Knight <vaughan@vaughanknight.com>
2017-04-23 13:51:55 +1000

    Updates for demo
    Added skybox

---
+++ RadialControllerProject/Assets/Hover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour {
+
+    public float VerticalMovement = 1f;
+    public float Speed = 1f;
+
+    private Vector3 _startPos;
+    private Transform _thisTransform;
+    
+	// Use this for initialization
+	void Start () {
+        _thisTransform = this.transform;
+        _startPos = _thisTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var yOffset = VerticalMovement * Mathf.Sin(Time.timeSinceLevelLoad * Mathf.PI * 2 / Speed);
+
+        _thisTransform.position = new Vector3(_startPos.x, _startPos.y + yOffset, _startPos.z);
+	}
+}
---
+++ RadialControllerProject/Assets/TrackTarget.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackTarget : MonoBehaviour
+{
+    public Transform Target;
+    private Transform _thisTransform;
+    public float Speed = 0.1f;
+    public Transform Destination;
+
+	// Use this for initialization
+	void Start () {
+        _thisTransform = this.transform;
+        
+        iTween.LookTo(gameObject, iTween.Hash("looktarget", Target.transform.position,
+            "time", 4f,
+            "oncomplete", "OnLookComplete",
+//            "delay", 2f,
+            "easetype", iTween.EaseType.easeInOutQuart));
+    }
+
+    public void OnLookComplete()
+    {
+        _Track = true;
+        iTween.MoveTo(gameObject, 
+            iTween.Hash("position", Destination.transform.position,
+                        "time", 4f,
+                        "easetype", iTween.EaseType.easeInOutQuart));
+    }
+
+    private bool _Track = false;
+    
+    // Update is called once per frame
+    void Update () {
+        
+        
+    }
+
+    void LateUpdate()
+    {
+        if (_Track)
+        {
+            _thisTransform.LookAt(Target);
+        }
+    }
+}
